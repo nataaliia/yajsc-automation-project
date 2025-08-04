51c43ffae3bf2573bd4526ee00410af307e0ec29
natasha <natashakuch808@gmail.com>
2025-08-04 23:02:33 +0300

    second commit - fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL:'https://practicesoftwaretesting.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     headless: false,
+    testIdAttribute:'data-test',
   },
 
   /* Configure projects for major browsers */
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,12 +8,12 @@ const user={
 }
 
 test('login', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
-  await page.locator('[data-test="email"]').fill(user.standardUser.email);
-  await page.locator('[data-test="password"]').fill(user.standardUser.password);
-  await page.locator('[data-test="login-submit"]').click();
+  await page.goto('/auth/login');
+  await page.getByTestId('email').fill(user.standardUser.email);
+  await page.getByTestId('password').fill(user.standardUser.password);
+  await page.getByTestId('login-submit').click();
 
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
-  await expect(page.locator('[data-test="page-title"]')).toHaveText('My account');
-  await expect(page.locator('[data-test="nav-menu"]')).toHaveText('Jane Doe');
+  await expect(page).toHaveURL(/\/account$/);
+  await expect(page.getByTestId('page-title')).toHaveText('My account');
+  await expect(page.getByTestId('nav-menu')).toHaveText('Jane Doe');
 });
