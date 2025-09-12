c8ae0a498ffcfbc6bbd4c707aca41e423a554ac6
natasha <natashakuch808@gmail.com>
2025-09-13 01:12:13 +0300

    fixes after review

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,9 +2,14 @@ import { loggedInUser as test, expect } from '../fixtures/loggedInApp.fixture';
 import { baseConfig } from '../config/baseConfig';
 
 test('login via loggedInUser fixture', async ({ app }) => {
-  await app.page.goto('/account');
-
-  await expect(app.page).toHaveURL(/\/account$/);
-  await expect(app.account.pageTitle).toHaveText('My account');
-  await expect(app.account.header.navMenu).toHaveText(baseConfig.USER_NAME);
+  await test.step('Open account page', async () => {
+    await app.page.goto('/account');
+    await expect(app.page).toHaveURL(/\/account$/);
+  });
+  await test.step('Verify account page title is visible', async () => {
+    await expect(app.account.pageTitle).toHaveText('My account');
+  });
+  await test.step('Verify username in the navigation menu', async () => {
+    await expect(app.account.header.navMenu).toHaveText(baseConfig.USER_NAME);
+  });
 });
